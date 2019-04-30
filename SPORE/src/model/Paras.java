98c86cd1dc05c0ce0df62ea1ac25d85fed1cc178
KiyomiHan <1033138571@qq.com>
2019-04-30 13:01:59 +0800

    fix auth bug

---
+++ SPORE/src/model/Paras.java
@@ -0,0 +1,24 @@
+package model;
+import utility.Statistics;
+
+public class Paras {
+    public static final double distance_threshold=100;//decide whether the threshold of the nearby items
+//    public static int countU=2189;
+//    public static int countV=500000;
+//    public static int countU=576;
+//    public static int countV=374830;
+//    public static int countU=87;
+//    public static int countV=247643;
+    public static int countU=1250;
+    public static int countV=9764;
+    public static int k = 5;
+    public static int K = 140;
+    //public static String dataset="synthetic";
+    public static String dataset="foursqure";
+//    public static int countR;
+    public static final int MAXTHREADSL=20;//64
+    public static final int MAXTHREADSM = 20; //32
+    public static final int MAXTHREADSS=1;//20
+    public static final double termate=1e-3;
+}
+
